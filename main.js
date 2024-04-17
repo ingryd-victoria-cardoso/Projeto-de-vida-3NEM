20eb91a3c0dce4ad090703720815b645bf64447c
ingryd-victoria-cardoso <129208186+ingryd-victoria-cardoso@users.noreply.github.com>
2024-04-17 15:43:54 -0300

    s

--- main.js
+++ main.js
@@ -14,3 +14,7 @@ for(let i=0;i <botoes.length;i++) {
    }
  }
     
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1= new Date()
+
+contadores[0].textContent ="contagem regressiva";
