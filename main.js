87c56a1df2cd651cf8294d723e7f866b38544f1b
ingryd-victoria-cardoso <129208186+ingryd-victoria-cardoso@users.noreply.github.com>
2024-04-17 15:29:59 -0300

    s

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const botoes = document.querySelectorAll(."botao");
-
+ const-textos =document.querySelectorAll(".aba-conteudo");
 for(let i=0;i <botoes.length;i++) {
  botoes[i].onclick = function(){
 
- for (let j=0;j<botoes.length;j++){
+ 
+   for (let j=0;j<botoes.length;j++){
     botoes{j}.classlit.remove("ativo");
+    textos{j}.classlit.remove("ativo");
+   }
     
-    
-    botoes(i).classlit.add("ativo");
- }
+    botoes[i].classlit.add("ativo");
+    textos[i].classlit.add("ativo");
+   }
  }
     
