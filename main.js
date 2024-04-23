a3852a6b4a01e6c0b0b3a377913b46b4239c76d7
ingryd-victoria-cardoso <129208186+ingryd-victoria-cardoso@users.noreply.github.com>
2024-04-23 15:13:02 -0300

    s

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0;i <botoes.length;i++) {
  }
     
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date("2024-10-05T00:00:00")
+const tempoObjetivo1= new Date("2023-10-05T00:00:00")
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -26,5 +26,6 @@ let tempoFinal = tempoObjetivo-tempoAtual;
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor (segundos/60);
 let horas =Math.floor (minutos/60);
-  return minutos;
+let dias = Math.floor(horas/24 );
+return dias +"dias"+ horas +"horas"+ minutos +"minutos"+ segundos +"segundos";
 }
