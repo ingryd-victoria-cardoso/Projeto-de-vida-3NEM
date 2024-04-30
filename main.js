d6b37687946b01fd2bb9e9796bb77ec9068accaa
ingryd-victoria-cardoso <129208186+ingryd-victoria-cardoso@users.noreply.github.com>
2024-04-30 14:23:02 -0300

    s

--- main.js
+++ main.js
@@ -23,7 +23,9 @@ const tempoObjetivo4 = new Date("2025-02-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-
+for (let i=0;i<contadores.length;i++){
+     contadores[i].textContent = calculaTempo(tempos[i]);
+}
 
 
   function calculaTempo(tempoObjetivo) {
